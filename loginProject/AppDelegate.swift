e1b7e33d48957b61e9750842a2db2af3789cd611
Ivan Campos <ivano.cs89@gmail.com>
2023-03-28 18:09:44 -0600

    Initial Commit

---
+++ loginProject/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  loginProject
+//
+//  Created by ISSC_412_2023 on 28/03/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ loginProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  loginProject
+//
+//  Created by ISSC_412_2023 on 28/03/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
